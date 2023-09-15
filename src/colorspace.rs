f3de896e046322ff8ba3b2a00d8477f7dced6b61
Kenny Pitt <kenny.pitt@microfocus.com>
2023-09-15 13:44:07 -0400

    Fix `interpolate` function tests
    This test case was actually testing the `interpolate_angle` function,
    not the `interpolate` function.

--- src/colorspace.rs
+++ src/colorspace.rs
@@ -44,10 +44,10 @@ fn interpolate_angle(a: Scalar, b: Scalar, fraction: Fraction) -> Scalar {
 
 #[test]
 fn test_interpolate() {
-    assert_eq!(0.0, interpolate_angle(0.0, 90.0, Fraction::from(0.0)));
-    assert_eq!(45.0, interpolate_angle(0.0, 90.0, Fraction::from(0.5)));
-    assert_eq!(90.0, interpolate_angle(0.0, 90.0, Fraction::from(1.0)));
-    assert_eq!(90.0, interpolate_angle(0.0, 90.0, Fraction::from(1.1)));
+    assert_eq!(0.0, interpolate(0.0, 0.5, Fraction::from(0.0)));
+    assert_eq!(0.25, interpolate(0.0, 0.5, Fraction::from(0.5)));
+    assert_eq!(0.5, interpolate(0.0, 0.5, Fraction::from(1.0)));
+    assert_eq!(0.5, interpolate(0.0, 0.5, Fraction::from(1.1)));
 }
 
 #[test]
